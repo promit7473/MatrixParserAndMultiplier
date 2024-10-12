fde7c0e65ada4074f19fa7f1ec782d3e6d60cee3
Promit7473 <merajhossainpromit@gmail.com>
2024-10-12 17:54:05 +0600

    updated

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mhpromit7473/CLionProjects/matrix_multiplier
+# Install script for directory: /home/mhpromit7473/MatrixParserAndMultiplier
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mhpromit7473/CLionProjects/matrix_multiplier/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mhpromit7473/MatrixParserAndMultiplier/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
